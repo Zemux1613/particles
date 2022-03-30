170c392bd0400e2f3e246800dc4d843d563a4f35
Tom L <devzemux1613@gmail.com>
2022-03-30 14:37:08 +0200

    feat: init project

---
+++ src/main/java/de/tom/particle/Particle.java
@@ -0,0 +1,17 @@
+package de.tom.particle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Particle extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
